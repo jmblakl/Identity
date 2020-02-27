45704c0300134d93cc7cd0b4de5483c2f7b65955
Justin Blakley <justinblakley@justins-mbp.lan>
2020-02-26 22:04:55 -0600

    initial commit

---
+++ src/Api/Controllers/IdentityController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+
+[Route("identity")]
+[Authorize]
+public class IdentityController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+    }
+}
